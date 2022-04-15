30f75cd84b94e3bb50ee8d6dc2ddc59e5bf5a240
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-04-15 16:22:34 +0300

    ewf

---
+++ whats-down/src/components/InputLine.js
@@ -0,0 +1,12 @@
+
+const InputLine = ({ label,type,placeholder,id, }) => {
+    return (
+
+        <div class="form-floating mb-3">
+            <input type={type} class="form-control" id={id} placeholder={placeholder}/>
+                <label for={id}>{label}</label>
+        </div>
+    )
+}
+
+export default InputLine
--- whats-down/src/components/Register.js
+++ whats-down/src/components/Register.js
@@ -1,3 +1,16 @@
 //input checks
 //save user
 //return to login
+import InputLine from "./InputLine"
+const Register = () => {
+    return (
+        <div>Register Form
+
+            <InputLine label='UserName:' type='email' id='userName' placeholder='Enter UserName' />
+
+        </div>
+
+    )
+}
+
+export default Register
