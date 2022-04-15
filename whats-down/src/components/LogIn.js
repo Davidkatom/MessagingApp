45b3981fe02bed4cfa7a76c04aaa1208f31312ba
Davidkatom <Davidkatom@gmail.com>
2022-04-15 16:57:15 +0300

    sdad

--- whats-down/src/components/LogIn.js
+++ whats-down/src/components/LogIn.js
@@ -1,3 +1,31 @@
 //user validation
 //start register
 //lunch chatdowns
+import PropTypes from 'prop-types'
+import InputLine from './InputLine';
+import Button from './Button';
+
+const logins = {a:1,b:2}
+
+const Login = () => {
+    return (
+        <div>
+            <div>                
+                <InputLine label='Username' placeholder='Username' type='text' id = 'username'/>
+                <InputLine label='Password' placeholder='Password' type='password' id = 'password'/>
+                <Button label = 'Login' classy="btn btn-primary" onClick = {logBu} id = 'login_button'/>
+            </div>
+        </div>
+    )
+}
+const logBu = function loginButton(){
+    console.log("Click")
+    if(logins[document.getElementById('username').value] == document.getElementById('password').value){
+        console.log("Login Successful")
+    }
+    else{
+        console.log("Login Failed")
+    }
+}
+
+export default Login
