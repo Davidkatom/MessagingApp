374aac7ec7db92a42a9edd2dab40274f5564a6ea
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-04-16 10:33:54 +0300

    omer

--- whats-down/src/App.js
+++ whats-down/src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
 import Login from './components/Login';
+import Register from './components/Register';
+import { useState } from 'react';
+
 
 function App() {
+  //user list
+  const [user_list, setUser] = useState([
+    {
+      user_name: 'omer',
+      password: '123',
+      display_name: 'hamdiTools',
+      picture: 'funny',
+    }
+  ]);
+
+  //add a new user to the user list
+  const addUser = (args) => {
+    setUser([...user_list, {
+      user_name: args.user_name,
+      password: args.password,
+      display_name: args.display_name,
+      picture: args.picture,
+    }])
+    return 'success'
+  }
+
+  //check if user already in userlist
+  const checkUser = (user_name) => {
+    let isExists = false;
+    user_list.map((user) => (
+      (user.user_name === user_name) ? isExists = true : 'good'
+    ))
+    return isExists
+  }
+
   return (
     <div className="App">
       <Login />
+      <Register user_list={user_list} checkUser={checkUser} addUser={addUser} />
     </div>
   );
 }
