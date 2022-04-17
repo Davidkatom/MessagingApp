2d5cf926809f8ad37f34fe8b418aab3acfd44a6d
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-04-17 22:15:02 +0300

    asdasda

--- whats-down/src/components/ChatScreen.js
+++ whats-down/src/components/ChatScreen.js
@@ -10,43 +10,50 @@ const ChatScreen = () => {
         {
             contact_name: 'omer',
             chat_history: ["hello", "hi", "how are you"],
-            last_message: 'empty chat',
+            last_message: 'Omer like to talk about his life',
+            last_message_time: '2 minutes ago',
         },
         {
             contact_name: 'david',
             chat_history: ["good morning", "bye", "fuck you"],
-            last_message: 'empty chat',
+            last_message: 'David loves to eat food and drink beer',
+            last_message_time: 'empty time',
         },
         {
             contact_name: 'joe',
             chat_history: ["good morning", "bye", "fuck you"],
             last_message: 'empty chat',
-            last_message_time: 'empty chat',
+            last_message_time: 'empty time',
         },
         {
             contact_name: 'yossi',
             chat_history: ["good morning", "bye", "fuck you"],
             last_message: 'empty chat',
+            last_message_time: 'empty time',
         },
         {
             contact_name: 'Hampti',
             chat_history: ["good morning", "bye", "fuck you"],
             last_message: 'empty chat',
+            last_message_time: 'empty time',
         },
         {
             contact_name: 'Dampti',
             chat_history: ["good morning", "bye", "fuck you"],
             last_message: 'empty chat',
+            last_message_time: 'empty time',
         },
         {
             contact_name: 'Tidididam',
             chat_history: ["good morning", "bye", "fuck you"],
             last_message: 'empty chat',
+            last_message_time: 'empty time',
         },
         {
             contact_name: 'UmcoolTum',
             chat_history: ["good morning", "bye", "fuck you"],
             last_message: 'empty chat',
+            last_message_time: 'empty time',
         }
     ]);
 
@@ -54,7 +61,7 @@ const ChatScreen = () => {
         <div class='container large'>
             <div chat-screen>header</div>
             <div class="row row-chat">
-                <div class="col-sm scrollable" >
+                <div class="col-5 scrollable" >
                     <div class='direction-fix'>
                         {contact_list.map((contactIndex) => (
                             <MiniContant contact={contactIndex} />
