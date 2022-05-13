fe622d924c35f1777364c5638eeee8c5c158705f
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-05-13 09:51:26 +0300

    recover project 2

---
+++ WebApplication2/wwwroot/js/search.js
@@ -0,0 +1,26 @@
+﻿//$('tbody').load('/Reviews/Search2?query=' + q)
+
+$(function () {
+
+    $('form').submit(async e => {
+        e.preventDefault();
+        const q = $('#SearchQuery').val();
+        var response = await fetch('Reviews/SearchToJson?query=' + q);
+        var data = await response.json();
+
+        let template = $('#reviewRowTemplate').html();
+        let results = '';
+
+        for (var item in data) {
+            let row = template;
+            for (var key in data[item]) {
+                row = row.replaceAll('{' + key + '}', data[item][key]);
+
+            }
+            results += row;
+        }
+        $('tbody').html(results);
+
+    })
+
+});
