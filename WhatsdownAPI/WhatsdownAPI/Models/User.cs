e5141d5f1130d9f89f288409209d4fbb9f39c8ee
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-05-19 16:11:27 +0300

    a

---
+++ WhatsdownAPI/WhatsdownAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace WhatsdownAPI.Models
+{
+    public class User
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? NickName { get; set; }
+        public string? ProfilePicture { get; set; }
+        public Contacts? ContactList { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+    }
+}
