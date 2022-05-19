c0a9100a3d0857c47bc87faf178358f28d5d4abb
Davidkatom <46295877+Davidkatom@users.noreply.github.com>
2022-05-19 23:02:38 +0300

    sdfsf

---
+++ WhatsdownAPI/WhatsdownAPI/Controllers/ContactsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhatsdownAPI;
+using WhatsdownAPI.Data;
+
+namespace WhatsdownAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly WhatsdownAPIContext _context;
+
+        public ContactsController(WhatsdownAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contacts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contacts>>> GetContacts()
+        {
+          if (_context.Contacts == null)
+          {
+              return NotFound();
+          }
+            return await _context.Contacts.ToListAsync();
+        }
+
+        // GET: api/Contacts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contacts>> GetContacts(int id)
+        {
+          if (_context.Contacts == null)
+          {
+              return NotFound();
+          }
+            var contacts = await _context.Contacts.FindAsync(id);
+
+            if (contacts == null)
+            {
+                return NotFound();
+            }
+
+            return contacts;
+        }
+
+        // PUT: api/Contacts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContacts(int id, Contacts contacts)
+        {
+            if (id != contacts.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contacts).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contacts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Contacts>> PostContacts(Contacts contacts)
+        {
+          if (_context.Contacts == null)
+          {
+              return Problem("Entity set 'WhatsdownAPIContext.Contacts'  is null.");
+          }
+            _context.Contacts.Add(contacts);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContacts", new { id = contacts.Id }, contacts);
+        }
+
+        // DELETE: api/Contacts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContacts(int id)
+        {
+            if (_context.Contacts == null)
+            {
+                return NotFound();
+            }
+            var contacts = await _context.Contacts.FindAsync(id);
+            if (contacts == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contacts.Remove(contacts);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ContactsExists(int id)
+        {
+            return (_context.Contacts?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
