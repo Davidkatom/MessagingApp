033aacb417b0d42e4f9e23d23a1aa047f16e78fc
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-05-23 13:58:32 +0300

    sql finished 2

--- WhatsdownAPI/WhatsdownAPI/Models/ContactRelation.cs
+++ WhatsdownAPI/WhatsdownAPI/Models/ContactRelation.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public User Contacter { get; set; }
         public User Contacted { get; set; }
+        public string ContactedNickName { get; set; }
+
     }
 }
