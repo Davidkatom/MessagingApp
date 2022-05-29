32984ce0aacdadb755dce810e774c50d24c206ee
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-05-29 10:57:22 +0300

    timedate and add contact fixed

--- whats-down/src/functions/TimeStempCalc.js
+++ whats-down/src/functions/TimeStempCalc.js
@@ -1,22 +1,27 @@
-import { isValidDateValue } from "@testing-library/user-event/dist/utils";
 
 function sqlToJsDate(sqlDate){
     let dateTimeParts= sqlDate.split(/[- : //]/); // regular expression split that creates array with: year, month, day, hour, minutes, seconds values
-    return new Date(parseInt(dateTimeParts[2]), parseInt(dateTimeParts[1])-1, parseInt(dateTimeParts[0])-1, parseInt(dateTimeParts[3]), parseInt(dateTimeParts[4]), parseInt(dateTimeParts[5]));
+    return new Date(parseInt(dateTimeParts[2]), parseInt(dateTimeParts[1])-1, parseInt(dateTimeParts[0]), parseInt(dateTimeParts[3]), parseInt(dateTimeParts[4]), parseInt(dateTimeParts[5]));
 }
 function fixDateWithT(dateWithT) {
     let dateTimeParts= dateWithT.split(/[- : // T .]/); // regular expression split that creates array with: year, month, day, hour, minutes, seconds values
-    return new Date(parseInt(dateTimeParts[0]), parseInt(dateTimeParts[1])-1, parseInt(dateTimeParts[2])-1, parseInt(dateTimeParts[3]), parseInt(dateTimeParts[4]), parseInt(dateTimeParts[5]));
+    return new Date(parseInt(dateTimeParts[0]), parseInt(dateTimeParts[1])-1, parseInt(dateTimeParts[2]), parseInt(dateTimeParts[3]), parseInt(dateTimeParts[4]), parseInt(dateTimeParts[5]));
 }
 
 export default function TimeStempCalc(timeStamp) {
     try{
-        let a = timeStamp.toLocaleDateString();
+        timeStamp.toLocaleDateString();
     }catch{
-        if(timeStamp.includes("T")){
-            timeStamp = fixDateWithT(timeStamp);
-        }else{
-            timeStamp = sqlToJsDate(timeStamp);
+        try{
+            fixDateWithT(timeStamp).toLocaleDateString();
+            timeStamp = fixDateWithT(timeStamp)
+        }catch{
+            try{
+                sqlToJsDate(timeStamp).toLocaleDateString();
+                timeStamp = sqlToJsDate(timeStamp)
+            }catch{
+                return "just now";
+            }
         }
     }
     var date = timeStamp == null ? '' : timeStamp.toLocaleDateString() === new Date().toLocaleDateString() ?
