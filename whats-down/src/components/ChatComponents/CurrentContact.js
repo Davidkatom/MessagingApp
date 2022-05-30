31b06a8b5b9fc8378820e6c3ea3373a5e74ec68b
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-05-30 09:44:12 +0300

    Update CurrentContact.js

--- whats-down/src/components/ChatComponents/CurrentContact.js
+++ whats-down/src/components/ChatComponents/CurrentContact.js
@@ -1,16 +1,18 @@
-
 const CurrentContact = ({ contact }) => {
-    console.log("pic:")
-    let minipici = contact().picture;
-    if (minipici === undefined) {
-        minipici = "blank-profile-picture.png";
+    let minipici = "blank-profile-picture.png";
+    if (contact() !== undefined) {
+        if (contact().picture !== undefined) {
+            minipici = contact().picture;
+        }
+        return (
+            <div className="col-7 contact-header">
+                <img src={minipici} className="float-start img-thumbnail rounded-start right-padding-for-picture collapse" alt="..."  id="selected-contact-image"/>
+                {contact().name}
+            </div>
+        )
+
     }
-    return (
-        <div className="col-7 contact-header">
-            <img src={minipici} className="float-start img-thumbnail rounded-start right-padding-for-picture collapse" alt="..."  id="selected-contact-image"/>
-            {contact().name}
-        </div>
-    )
+    return (<div></div>)
 }
 
 export default CurrentContact
