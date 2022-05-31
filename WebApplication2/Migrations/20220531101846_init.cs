bfb11414dbe6f68df74971ffc8103c8c314b2dc8
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-05-31 14:33:47 +0300

    reviews job up

--- WebApplication2/Migrations/20220531101846_init.cs
+++ WebApplication2/Migrations/20220531101846_init.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace WebApplication2.Migrations
 {
-    public partial class Init : Migration
+    public partial class init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
--- WebApplication2/wwwroot/js/search.js
+++ WebApplication2/wwwroot/js/search.js
@@ -6,10 +6,13 @@ $(function () {
         const q = $('#SearchQuery').val();
         var response = await fetch('Reviews/SearchToJson?query=' + q);
         var data = await response.json();
-
+        //update the reviews table:
         let template = $('#reviewRowTemplate').html();
         let results = '';
+        let sumOfRate = 0;
         for (var item in data) {
+            sumOfRate+=data[item].rate
+            //sumOfRate +=item[]
             let row = template;
             for (var key in data[item]) {
                 row = row.replaceAll('{' + key + '}', data[item][key]);
@@ -18,6 +21,11 @@ $(function () {
             results += row;
         }
         $('tbody').html(results);
+        //update the current rating:
+        //let template = $('#reviewRowTemplate').html();
+
+        let str = sumOfRate.toString();
+        $('#scoreSide').html("Avarage Rating of showed reviews is: "+ str);
 
     })
 
