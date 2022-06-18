2fdc1dbe937a0858fdc7856771d32b3a9c9eb483
Omerlf208 <74273544+HamdiTools@users.noreply.github.com>
2022-06-18 10:36:09 +0300

    asdaas

--- ex3/app/src/main/java/com/example/myapplication/LoginScreen.java
+++ ex3/app/src/main/java/com/example/myapplication/LoginScreen.java
@@ -1,13 +1,18 @@
 package com.example.myapplication;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class LoginScreen extends AppCompatActivity {
+    private static final String CHANNEL_ID = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +37,39 @@ public class LoginScreen extends AppCompatActivity {
             Intent i = new Intent(this, ContactScreen.class);
             startActivity(i);
         });
+
+        //notification manager
+        createNotificationChannel();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        Button btnPushNotification = findViewById(R.id.tempPushNotification);
+        btnPushNotification.setOnClickListener(v->{
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle("My notification")
+                    .setContentText("Hello World!")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true);//makes the notification disappear when clicked
+            int notificationId = 1; // id of notification (unique) - can be used to cancel or update notification
+            notificationManager.notify(notificationId, builder.build());
+            //16:40 how to use notification manager to go to new intent
+            //22:00 updating notification
+
+
+        });
+    }
+
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = getString(R.string.Channel_main_name);
+            String description = getString(R.string.Channel_main_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+            //here we can add more channels if needed like 07:20
+        }
     }
 }
